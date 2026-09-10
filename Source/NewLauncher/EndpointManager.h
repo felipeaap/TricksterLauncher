18a45029de36a22e9bf53ccedefa0bb82fd31d19
Felipe Augusto Aguiar Plentz <felipeplentz0@gmail.com>
2026-09-10 14:42:00 -0300

    refactor: add update endpoint manager

---
+++ Source/NewLauncher/EndpointManager.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "DownloadManager.h"
+
+class EndpointManager
+{
+public:
+    EndpointManager(std::vector<std::string> hosts,
+                    bool useSsl,
+                    DownloadManager::Options options = {});
+
+    std::string Get(const std::string& path) const;
+    bool Download(const std::string& remotePath,
+                  const std::string& localPath,
+                  DownloadManager::ProgressCallback progress = {},
+                  DownloadManager::SpeedCallback speed = {}) const;
+
+private:
+    std::vector<std::string> hosts_;
+    bool useSsl_;
+    DownloadManager::Options options_;
+};
