a8cbf90d7880ead1ea2c311999eaf18c626349c3
Felipe Augusto Aguiar Plentz <felipeplentz0@gmail.com>
2026-09-10 17:28:39 -0300

    refactor: make LauncherWindow own native window implementation

--- Source/NewLauncher/LauncherWindow.cpp
+++ Source/NewLauncher/LauncherWindow.cpp
@@ -5,39 +5,157 @@
 
 namespace
 {
-constexpr wchar_t kWindowClassName[] = L"class001";
 constexpr int kWebViewLeft = 19;
 constexpr int kWebViewTop = 32;
 constexpr int kWebViewWidth = 503;
 constexpr int kWebViewHeight = 343;
+}
 
-LRESULT CALLBACK WindowProcess(HWND window, UINT message, WPARAM wideParameter, LPARAM longParameter)
+LauncherWindow::~LauncherWindow()
 {
-    if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter))
-        return true;
+    Destroy();
+}
 
+bool LauncherWindow::Create(const wchar_t* title) noexcept
+{
+    if (!title || window_)
+        return false;
+
+    instance_ = GetModuleHandleW(nullptr);
+
+    WNDCLASSEXW windowClass{};
+    windowClass.cbSize = sizeof(windowClass);
+    windowClass.style = CS_CLASSDC;
+    windowClass.lpfnWndProc = &LauncherWindow::WindowProcess;
+    windowClass.hInstance = instance_;
+    windowClass.lpszClassName = kWindowClassName;
+
+    if (!RegisterClassExW(&windowClass))
+    {
+        if (GetLastError() != ERROR_CLASS_ALREADY_EXISTS)
+            return false;
+    }
+    else
+    {
+        classRegistered_ = true;
+    }
+
+    window_ = CreateWindowExW(
+        0,
+        kWindowClassName,
+        title,
+        WS_POPUP,
+        100,
+        100,
+        gui::WIDTH,
+        gui::HEIGHT,
+        nullptr,
+        nullptr,
+        instance_,
+        this);
+
+    if (!window_)
+    {
+        if (classRegistered_)
+        {
+            UnregisterClassW(kWindowClassName, instance_);
+            classRegistered_ = false;
+        }
+        return false;
+    }
+
+    running_ = true;
+    ShowWindow(window_, SW_SHOWDEFAULT);
+    UpdateWindow(window_);
+
+    // Temporary compatibility bridge for the remaining GUI layer. Window
+    // ownership itself remains entirely inside LauncherWindow.
+    gui::window = window_;
+    gui::windowClass.hInstance = instance_;
+    gui::windowClass.lpszClassName = kWindowClassName;
+
+    return true;
+}
+
+void LauncherWindow::Destroy() noexcept
+{
+    const HWND destroyedWindow = window_;
+
+    if (window_)
+    {
+        DestroyWindow(window_);
+        window_ = nullptr;
+    }
+
+    if (classRegistered_ && instance_)
+    {
+        UnregisterClassW(kWindowClassName, instance_);
+        classRegistered_ = false;
+    }
+
+    if (gui::window == destroyedWindow)
+        gui::window = nullptr;
+
+    instance_ = nullptr;
+    running_ = false;
+}
+
+bool LauncherWindow::PumpMessages() noexcept
+{
+    MSG message{};
+    while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
+    {
+        if (message.message == WM_QUIT)
+        {
+            running_ = false;
+            gui::isRunning = false;
+            return false;
+        }
+
+        TranslateMessage(&message);
+        DispatchMessage(&message);
+    }
+
+    return running_ && gui::isRunning;
+}
+
+LRESULT CALLBACK LauncherWindow::WindowProcess(
+    HWND window,
+    UINT message,
+    WPARAM wideParameter,
+    LPARAM longParameter)
+{
+    auto* self = reinterpret_cast<LauncherWindow*>(
+        GetWindowLongPtrW(window, GWLP_USERDATA));
+
+    if (message == WM_NCCREATE)
+    {
+        const auto* createStruct = reinterpret_cast<CREATESTRUCTW*>(longParameter);
+        self = static_cast<LauncherWindow*>(createStruct->lpCreateParams);
+        SetWindowLongPtrW(window, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(self));
+    }
+
+    if (ImGui_ImplWin32_WndProcHandler(window, message, wideParameter, longParameter))
+        return TRUE;
+
+    if (self)
+        return self->HandleMessage(message, wideParameter, longParameter);
+
+    return DefWindowProcW(window, message, wideParameter, longParameter);
+}
+
+LRESULT LauncherWindow::HandleMessage(UINT message, WPARAM wideParameter, LPARAM longParameter) noexcept
+{
     switch (message)
     {
     case WM_SIZE:
-        if (gui::device && wideParameter != SIZE_MINIMIZED)
-        {
-            gui::presentParameters.BackBufferWidth = LOWORD(longParameter);
-            gui::presentParameters.BackBufferHeight = HIWORD(longParameter);
-            gui::ResetDevice();
-        }
+        if (wideParameter != SIZE_MINIMIZED && resizeCallback_)
+            resizeCallback_(LOWORD(longParameter), HIWORD(longParameter));
         return 0;
 
     case WM_MOVE:
-        if (gui::g_controller)
-        {
-            RECT bounds = {
-                kWebViewLeft,
-                kWebViewTop,
-                kWebViewLeft + kWebViewWidth,
-                kWebViewTop + kWebViewHeight
-            };
-            gui::g_controller->put_Bounds(bounds);
-        }
+        if (moveCallback_)
+            moveCallback_();
         break;
 
     case WM_SYSCOMMAND:
@@ -46,12 +164,13 @@ LRESULT CALLBACK WindowProcess(HWND window, UINT message, WPARAM wideParameter, 
         break;
 
     case WM_DESTROY:
+        running_ = false;
         gui::isRunning = false;
         PostQuitMessage(0);
         return 0;
 
     case WM_LBUTTONDOWN:
-        gui::position = MAKEPOINTS(longParameter);
+        dragOrigin_ = MAKEPOINTS(longParameter);
         return 0;
 
     case WM_MOUSEMOVE:
@@ -59,15 +178,15 @@ LRESULT CALLBACK WindowProcess(HWND window, UINT message, WPARAM wideParameter, 
         {
             const POINTS points = MAKEPOINTS(longParameter);
             RECT rect{};
-            GetWindowRect(gui::window, &rect);
-            rect.left += points.x - gui::position.x;
-            rect.top += points.y - gui::position.y;
+            GetWindowRect(window_, &rect);
+            rect.left += points.x - dragOrigin_.x;
+            rect.top += points.y - dragOrigin_.y;
 
-            if (gui::position.x >= 0 && gui::position.x <= gui::WIDTH &&
-                gui::position.y >= 0 && gui::position.y <= 19)
+            if (dragOrigin_.x >= 0 && dragOrigin_.x <= gui::WIDTH &&
+                dragOrigin_.y >= 0 && dragOrigin_.y <= 19)
             {
                 SetWindowPos(
-                    gui::window,
+                    window_,
                     HWND_TOPMOST,
                     rect.left,
                     rect.top,
@@ -79,85 +198,5 @@ LRESULT CALLBACK WindowProcess(HWND window, UINT message, WPARAM wideParameter, 
         return 0;
     }
 
-    return DefWindowProc(window, message, wideParameter, longParameter);
-}
-}
-
-bool LauncherWindow::Create(const wchar_t* title) const noexcept
-{
-    if (!title)
-        return false;
-
-    gui::windowClass.cbSize = sizeof(WNDCLASSEXW);
-    gui::windowClass.style = CS_CLASSDC;
-    gui::windowClass.lpfnWndProc = WindowProcess;
-    gui::windowClass.cbClsExtra = 0;
-    gui::windowClass.cbWndExtra = 0;
-    gui::windowClass.hInstance = GetModuleHandleW(nullptr);
-    gui::windowClass.hIcon = nullptr;
-    gui::windowClass.hCursor = nullptr;
-    gui::windowClass.hbrBackground = nullptr;
-    gui::windowClass.lpszMenuName = nullptr;
-    gui::windowClass.lpszClassName = kWindowClassName;
-    gui::windowClass.hIconSm = nullptr;
-
-    if (!RegisterClassExW(&gui::windowClass))
-    {
-        if (GetLastError() != ERROR_CLASS_ALREADY_EXISTS)
-            return false;
-    }
-
-    gui::window = CreateWindowExW(
-        0,
-        kWindowClassName,
-        title,
-        WS_POPUP,
-        100,
-        100,
-        gui::WIDTH,
-        gui::HEIGHT,
-        nullptr,
-        nullptr,
-        gui::windowClass.hInstance,
-        nullptr);
-
-    if (!gui::window)
-    {
-        UnregisterClassW(kWindowClassName, gui::windowClass.hInstance);
-        return false;
-    }
-
-    ShowWindow(gui::window, SW_SHOWDEFAULT);
-    UpdateWindow(gui::window);
-    return true;
-}
-
-void LauncherWindow::Destroy() const noexcept
-{
-    if (gui::window)
-    {
-        DestroyWindow(gui::window);
-        gui::window = nullptr;
-    }
-
-    if (gui::windowClass.hInstance)
-        UnregisterClassW(kWindowClassName, gui::windowClass.hInstance);
-}
-
-bool LauncherWindow::PumpMessages() const noexcept
-{
-    MSG message{};
-    while (PeekMessage(&message, nullptr, 0, 0, PM_REMOVE))
-    {
-        if (message.message == WM_QUIT)
-        {
-            gui::isRunning = false;
-            return false;
-        }
-
-        TranslateMessage(&message);
-        DispatchMessage(&message);
-    }
-
-    return gui::isRunning;
+    return DefWindowProcW(window_, message, wideParameter, longParameter);
 }
