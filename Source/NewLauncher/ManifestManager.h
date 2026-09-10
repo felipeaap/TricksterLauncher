62c68968183973d3d9506e194a5c939bda73a159
Felipe Augusto Aguiar Plentz <felipeplentz0@gmail.com>
2026-09-10 14:25:48 -0300

    refactor: decouple manifest manager

--- Source/NewLauncher/ManifestManager.h
+++ Source/NewLauncher/ManifestManager.h
@@ -1,21 +1,23 @@
 #pragma once
 
+#include <functional>
 #include <string>
 #include <vector>
 
-#include "Helper.h"
+#include "UpdateTypes.h"
 
 class ManifestManager
 {
 public:
     using FileList = std::vector<Arquivo>;
+    using FetchFunction = std::function<std::string(const std::string&)>;
 
-    explicit ManifestManager(Helper& helper);
+    explicit ManifestManager(FetchFunction fetch);
 
     int Load(FileList& files, bool isFullCheck, int& localVersion);
 
 private:
-    Helper& helper_;
+    FetchFunction fetch_;
 
     static bool IsSamePath(const std::string& left, const std::string& right);
     static void MergeFile(FileList& files, const Arquivo& file);
