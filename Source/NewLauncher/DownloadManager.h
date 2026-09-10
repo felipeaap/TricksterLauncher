423c3758bfb942b8e83b0fbe7545d1c2abe98aa3
Felipe Augusto Aguiar Plentz <felipeplentz0@gmail.com>
2026-09-10 14:27:37 -0300

    refactor: add download manager interface

---
+++ Source/NewLauncher/DownloadManager.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <functional>
+#include <string>
+
+class DownloadManager
+{
+public:
+    using ProgressCallback = std::function<void(long long, long long)>;
+    using SpeedCallback = std::function<void(double)>;
+
+    DownloadManager(std::string host, bool useSsl);
+
+    std::string Get(const std::string& path) const;
+    bool Download(const std::string& remotePath,
+                  const std::string& localPath,
+                  ProgressCallback progress = {},
+                  SpeedCallback speed = {}) const;
+
+private:
+    std::string host_;
+    bool useSsl_;
+};
