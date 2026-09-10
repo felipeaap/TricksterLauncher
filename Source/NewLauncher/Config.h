1725afe14f82b288d690c5e0b8008a07e62c7ac0
Felipe Augusto Aguiar Plentz <felipeplentz0@gmail.com>
2026-09-10 14:42:09 -0300

    config: add CDN endpoint list

--- Source/NewLauncher/Config.h
+++ Source/NewLauncher/Config.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <string>
+#include <vector>
 
 namespace config
 {
-	extern std::wstring WindowTitle;
-	extern std::string SubTitle;
-	extern std::string LauncherCDN;
-	extern std::wstring BaseNewsURL;
-	extern std::string OptionExecName;
-	extern std::string WebsiteLink;
-	extern bool IsCDNUsingSSL;
-	extern bool IsDllInjectEnable;
-	extern std::string InjectDLLName;
+    extern std::wstring WindowTitle;
+    extern std::string SubTitle;
+    extern std::string LauncherCDN;
+    extern std::vector<std::string> LauncherCDNBackups;
+    extern std::wstring BaseNewsURL;
+    extern std::string OptionExecName;
+    extern std::string WebsiteLink;
+    extern bool IsCDNUsingSSL;
+    extern bool IsDllInjectEnable;
+    extern std::string InjectDLLName;
 }
